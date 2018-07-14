8a7016bed457b48bf8cf83c129ae909dab2ba23c
Saumay Paul <saumaypaul20@gmail.com>
2018-07-14 21:34:19 +0530

    Update Ass1.c

--- Ass1.c
+++ Ass1.c
@@ -1,22 +1,19 @@
+//CODE FOR VERTICAL OIL SEPERATOR
 #include<stdio.h>
 #include<math.h>
 int i,t;
 float p1,pg,dm,u,Vt,Re,cd1,d2,y,x,P,Z,Qg,T,Q,h,l,ratio,rounded_cd1,temp,d;
 float cd = 0.34;
 
-//3
-float Calculate_second(float r)
-{
+float Calculate_second(float r){
     	printf("CD= %0.3f\n",r);
     	y = ((pg/(p1-pg))*(r/dm));
 		d2= 5040*((T*Z*Qg)/P)*sqrt(y);
 		d = sqrt(d2);
 		d = round(d);
 	
-    	for(i=0;i<5; i++)
-		{
-			for(t=1;t<=4;t++)
-			{	
+    	for(i=0;i<5; i++){
+			for(t=1;t<=4;t++){	
 			
 			//calculate H
 			h = (t*Q)/(0.12*d*d);
@@ -30,8 +27,7 @@ float Calculate_second(float r)
 					
 					l=(h+d+40)/12;	
 				}
-				else
-				{
+				else{
 					l=(h+76)/12;	
 				}
 	 		
@@ -41,13 +37,11 @@ float Calculate_second(float r)
 				
 				ratio= (l*12)/d;
 				
-				if(ratio>3.0  && ratio < 4.0)
-					{
+				if(ratio>3.0  && ratio < 4.0){
 						printf("\nAcceptable L/D: %0.2f\n\n\n", ratio);
 					
 					}
-				else
-					{
+				else{
 						printf("\nUnacceptable L/D: %0.2f\n\n\n",ratio);
 					}
 
@@ -56,10 +50,8 @@ float Calculate_second(float r)
 		}
 
 }
-//2
 
-float Calculate_cd1(float f)
-{
+float Calculate_cd1(float f){
 	temp = f;
 	x = ((p1-pg)/pg) * (dm/temp);
 	Vt = 0.0119*sqrt(x);
@@ -81,10 +73,7 @@ float Calculate_cd1(float f)
 	
 }
 
-//1
-
-input()
-{
+input(){
 	printf("-------------------PROTOTYPE------------------------\n\n");
 	printf("-----WELCOME TO VERTICAL OIL SEPERATOR PROGRAM!-----\n\n");
 	printf("------------START INPUTTING THE VALUES--------------\n\n");
@@ -115,8 +104,6 @@ input()
 	
 	printf("Enter the  Q: ");
 	scanf("%f",&Q);
-	
-
 }
 
 void main()
